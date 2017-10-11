cb42398fc5ddc83d693c1e30a9e7d861452f54fb
JustOff <Off.Just.Off@gmail.com>
2017-10-11 15:14:23 +0300

    Add uMatrix support

--- bootstrap.js
+++ bootstrap.js
@@ -6,7 +6,9 @@ Cu.import("resource://gre/modules/AddonManager.jsm");
 
 const branch = "extensions.ublock0-updater.";
 const XMLHttpRequest = CC("@mozilla.org/xmlextras/xmlhttprequest;1","nsIXMLHttpRequest");
-const u0id = "uBlock0@raymondhill.net", u0Data = `<RDF:Description about="urn:mozilla:extension:uBlock0@raymondhill.net">
+const u0id = "uBlock0@raymondhill.net", u0dir = "uBlock", u0file = "uBlock0.firefox.xpi",
+      uMid = "uMatrix@raymondhill.net", uMdir = "uMatrix", uMfile = "uMatrix.firefox.xpi",
+      uData = `<RDF:Description about="urn:mozilla:extension:%APP%">
   <em:updates>
     <RDF:Seq>
       <RDF:li>
@@ -17,7 +19,7 @@ const u0id = "uBlock0@raymondhill.net", u0Data = `<RDF:Description about="urn:mo
               <em:id>{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}</em:id>
               <em:minVersion>27.0.0</em:minVersion>
               <em:maxVersion>27.*</em:maxVersion>
-              <em:updateLink>https://github.com/gorhill/uBlock/releases/download/%VERSION%/uBlock0.firefox.xpi</em:updateLink>
+              <em:updateLink>https://github.com/gorhill/%DIR%/releases/download/%VERSION%/%FILE%</em:updateLink>
             </RDF:Description>
           </em:targetApplication>
           <em:targetApplication>
@@ -25,7 +27,7 @@ const u0id = "uBlock0@raymondhill.net", u0Data = `<RDF:Description about="urn:mo
               <em:id>{92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}</em:id>
               <em:minVersion>2.40</em:minVersion>
               <em:maxVersion>*</em:maxVersion>
-              <em:updateLink>https://github.com/gorhill/uBlock/releases/download/%VERSION%/uBlock0.firefox.xpi</em:updateLink>
+              <em:updateLink>https://github.com/gorhill/%DIR%/releases/download/%VERSION%/%FILE%</em:updateLink>
             </RDF:Description>
           </em:targetApplication>
           <em:targetApplication>
@@ -33,7 +35,7 @@ const u0id = "uBlock0@raymondhill.net", u0Data = `<RDF:Description about="urn:mo
               <em:id>{ec8030f7-c20a-464f-9b0e-13a3a9e97384}</em:id>
               <em:minVersion>45.0</em:minVersion>
               <em:maxVersion>56.*</em:maxVersion>
-              <em:updateLink>https://github.com/gorhill/uBlock/releases/download/%VERSION%/uBlock0.firefox.xpi</em:updateLink>
+              <em:updateLink>https://github.com/gorhill/%DIR%/releases/download/%VERSION%/%FILE%</em:updateLink>
             </RDF:Description>
           </em:targetApplication>
           <em:targetApplication>
@@ -41,7 +43,7 @@ const u0id = "uBlock0@raymondhill.net", u0Data = `<RDF:Description about="urn:mo
               <em:id>{3550f703-e582-4d05-9a08-453d09bdfdc6}</em:id>
               <em:minVersion>45.0</em:minVersion>
               <em:maxVersion>*</em:maxVersion>
-              <em:updateLink>https://github.com/gorhill/uBlock/releases/download/%VERSION%/uBlock0.firefox.xpi</em:updateLink>
+              <em:updateLink>https://github.com/gorhill/%DIR%/releases/download/%VERSION%/%FILE%</em:updateLink>
             </RDF:Description>
           </em:targetApplication>
         </RDF:Description>
@@ -50,18 +52,26 @@ const u0id = "uBlock0@raymondhill.net", u0Data = `<RDF:Description about="urn:mo
   </em:updates>
 </RDF:Description>`;
 
-var u0Beta = false, u0Ver = "";
+var u0upd, uMupd, u0ver = "", uMver = "";
 
 var httpObserver = {
 	observe: function(subject, topic, data) {
 		if (topic == 'http-on-examine-response' || topic == 'http-on-examine-cached-response') {
 			subject.QueryInterface(Ci.nsIHttpChannel);
-			if (subject.URI.host == "versioncheck.addons.mozilla.org" 
-					&& subject.URI.path.indexOf("&id=" + u0id +"&") != -1) {
-				checkUpdate();
-				subject.QueryInterface(Ci.nsITraceableChannel);
-				var newListener = new TracingListener();
-				newListener.originalListener = subject.setNewListener(newListener);
+			if (subject.URI.host == "versioncheck.addons.mozilla.org") {
+				if (u0upd && subject.URI.path.indexOf("&id=" + u0id +"&") != -1) {
+					checkUpdate(u0id, u0dir);
+					subject.QueryInterface(Ci.nsITraceableChannel);
+					var newListener = new TracingListener();
+					newListener.app = u0id;
+					newListener.originalListener = subject.setNewListener(newListener);
+				} else if (uMupd && subject.URI.path.indexOf("&id=" + uMid +"&") != -1) {
+					checkUpdate(uMid, uMdir);
+					subject.QueryInterface(Ci.nsITraceableChannel);
+					var newListener = new TracingListener();
+					newListener.app = uMid;
+					newListener.originalListener = subject.setNewListener(newListener);
+				}
 			}
 		}
 	},
@@ -106,8 +116,14 @@ TracingListener.prototype = {
 	},
 	onStopRequest: function(request, context, statusCode) {
 		var upd = "", data = this.receivedData.join("");
-		if (u0Ver != "") {
-			upd = u0Data.replace(/%VERSION%/g, u0Ver);
+		if (this.app == u0id) {
+			if (u0ver != "") {
+				upd = uData.replace(/%VERSION%/g, u0ver).replace("%APP%", u0id).replace(/%DIR%/g, u0dir).replace(/%FILE%/g, u0file);
+			}
+		} else {
+			if (uMver != "") {
+				upd = uData.replace(/%VERSION%/g, uMver).replace("%APP%", uMid).replace(/%DIR%/g, uMdir).replace(/%FILE%/g, uMfile);
+			}
 		}
 		data = data.replace(/<RDF:Description[\s\S]*Description>/, upd);
 		var storageStream = CCIN("@mozilla.org/storagestream;1", "nsIStorageStream");
@@ -135,16 +151,30 @@ TracingListener.prototype = {
 
 var prefObserver = {
 	observe: function(subject, topic, data) {
-		if (topic == "nsPref:changed" && data == "u0Beta") {
-			u0Beta = Services.prefs.getBranch(branch).getBoolPref("u0Beta");
-			u0Ver = "";
-			AddonManager.getAllInstalls(function(aList) {
-				for (var addon of aList) {
-					if (addon.existingAddon && addon.existingAddon.id == u0id) {
-						addon.cancel();
+		if (topic != "nsPref:changed") return;
+		switch (data) {
+			case "uBlock0":
+				u0upd = Services.prefs.getBranch(branch).getIntPref("uBlock0");
+				u0ver = "";
+				AddonManager.getAllInstalls(function(aList) {
+					for (var addon of aList) {
+						if (addon.existingAddon && addon.existingAddon.id == u0id) {
+							addon.cancel();
+						}
 					}
-				}
-			})
+				});
+				break;
+			case "uMatrix":
+				uMupd = Services.prefs.getBranch(branch).getIntPref("uMatrix");
+				uMver = "";
+				AddonManager.getAllInstalls(function(aList) {
+					for (var addon of aList) {
+						if (addon.existingAddon && addon.existingAddon.id == uMid) {
+							addon.cancel();
+						}
+					}
+				});
+				break;
 		}
 	},
 	register: function() {
@@ -156,51 +186,73 @@ var prefObserver = {
 	}
 }
 
-function doUpdate(newVer) {
-	if (newVer != u0Ver) {
-		u0Ver = newVer;
-		AddonManager.getAddonByID(u0id, function(addon) {
-			addon.findUpdates({
-				onUpdateAvailable: function(aAddon, aInstall) {
-					if (aAddon.permissions & AddonManager.PERM_CAN_UPGRADE && AddonManager.shouldAutoUpdate(aAddon)) {
-						aInstall.install();
+function doUpdate(app, newVer) {
+	var isUpd = false;
+	if (app == u0id && newVer != u0ver) {
+		u0ver = newVer;
+		isUpd = true;
+	} else if (app == uMid && newVer != uMver) {
+		uMver = newVer;
+		isUpd = true;
+	}
+	if (isUpd) {
+		AddonManager.getAddonByID(app, function(addon) {
+			if (addon) {
+					addon.findUpdates({
+					onUpdateAvailable: function(aAddon, aInstall) {
+						if (aAddon.permissions & AddonManager.PERM_CAN_UPGRADE && AddonManager.shouldAutoUpdate(aAddon)) {
+							aInstall.install();
+						}
 					}
-				}
-			}, AddonManager.UPDATE_WHEN_USER_REQUESTED);
+				}, AddonManager.UPDATE_WHEN_USER_REQUESTED);
+			}
 		});
 	}
 }
 
-function checkUpdate() {
+function checkUpdate(app, dir) {
 	var ver, tag, request = new XMLHttpRequest();
-	if (u0Beta) {
-		request.open("GET", "https://github.com/gorhill/uBlock/releases");
+	if (u0upd == 1 || uMupd == 1) {
+		request.open("HEAD", "https://github.com/gorhill/" + dir + "/releases/latest");
+		request.onreadystatechange = function() {
+			if (this.readyState === this.DONE) {
+				if ((ver = /tag\/(\d+\.\d+\.\d+)$/.exec(this.responseURL)) !== null) {
+					doUpdate(app, ver[1]);
+				}
+			}
+		}
+	} else if (u0upd == 2 || uMupd == 2) {
+		request.open("GET", "https://github.com/gorhill/" + dir + "/releases");
 		request.responseType = "document";
 		request.onload = function() {
 			try {
 				tag = this.responseXML.getElementsByClassName("release-title")[0].getElementsByTagName("a")[0].href;
 				if ((ver = /tag\/(\d+\.\d+\.\w+)$/.exec(tag)) !== null) {
-					doUpdate(ver[1]);
+					doUpdate(app, ver[1]);
 				}
 			} catch (e) {}
-		}
-	} else {
-		request.open("HEAD", "https://github.com/gorhill/uBlock/releases/latest");
-		request.onreadystatechange = function() {
-			if (this.readyState === this.DONE) {
-				if ((ver = /tag\/(\d+\.\d+\.\d+)$/.exec(this.responseURL)) !== null) {
-					doUpdate(ver[1]);
-				}
-			}
 		}
 	}
 	request.send();
 }
 
 function startup(data, reason) {
-	try {
-		u0Beta = Services.prefs.getBranch(branch).getBoolPref("u0Beta");
+	var defaultBranch = Services.prefs.getDefaultBranch(branch);
+	defaultBranch.setIntPref("uBlock0", 1);
+	defaultBranch.setIntPref("uMatrix", 1);
+
+	var mainBranch = Services.prefs.getBranch(branch);
+	u0upd = mainBranch.getIntPref("uBlock0");
+	uMupd = mainBranch.getIntPref("uMatrix");
+
+	try { // migration, should be removed in next version
+		if (mainBranch.getBoolPref("u0Beta")) {
+			u0upd = 2;
+			mainBranch.setIntPref("uBlock0", u0upd);
+			mainBranch.clearUserPref("u0Beta");
+		}
 	} catch (e) {}
+
 	prefObserver.register();
 	httpObserver.register();
 }
